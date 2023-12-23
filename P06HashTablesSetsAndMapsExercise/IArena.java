b046a3cf281268eff80582eadb9fe8044b3e0c22
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2023-12-24 01:53:02 +0200

    update

---
+++ P06HashTablesSetsAndMapsExercise/IArena.java
@@ -0,0 +1,34 @@
+package P06HashTablesSetsAndMapsExercise;
+
+import P06HashTablesSetsAndMapsExercise.Battlecard;
+import P06HashTablesSetsAndMapsExercise.CardType;
+
+public interface IArena extends Iterable<Battlecard> {
+    void add(Battlecard card);
+
+    boolean contains(Battlecard card);
+
+    int count();
+
+    void changeCardType(int id, CardType type);
+
+    Battlecard getById(int id);
+
+    void removeById(int id);
+
+    Iterable<Battlecard> getByCardType(CardType type);
+
+    Iterable<Battlecard> getByTypeAndDamageRangeOrderedByDamageThenById(CardType type, int lo, int hi);
+
+    Iterable<Battlecard> getByCardTypeAndMaximumDamage(CardType type, double damage);
+
+    Iterable<Battlecard> getByNameOrderedBySwagDescending(String name);
+
+    Iterable<Battlecard> getByNameAndSwagRange(String name, double lo, double hi);
+
+    Iterable<Battlecard> getAllByNameAndSwag();
+
+    Iterable<Battlecard> findFirstLeastSwag(int n);
+
+    Iterable<Battlecard> getAllInSwagRange(double lo, double hi);
+}
