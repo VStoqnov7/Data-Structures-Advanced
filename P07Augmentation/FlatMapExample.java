18f58425a1241435f20b7c56e23fd99d3128336c
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2023-12-27 00:47:25 +0200

    update

---
+++ P07Augmentation/FlatMapExample.java
@@ -0,0 +1,22 @@
+package P07Augmentation;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FlatMapExample {
+    public static void main(String[] args) {
+        List<List<Integer>> listOfLists = Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(4, 5, 6),
+                Arrays.asList(7, 8, 9)
+        );
+
+        List<Integer> flattenedList = listOfLists.stream()
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+
+        System.out.println("Original List of Lists: " + listOfLists);
+        System.out.println("Flattened List: " + flattenedList);
+    }
+}
