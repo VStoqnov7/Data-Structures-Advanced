1e6b2d395ff93de17663bc95c3822887a0e7c63a
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2023-12-29 00:17:59 +0200

    update

---
+++ P08Exams/P02Exam/mlm/models/Seller.java
@@ -0,0 +1,11 @@
+package P08Exams.P02Exam.mlm.models;
+
+public class Seller {
+    public String id;
+    public int earnings;
+
+    public Seller(String id) {
+        this.id = id;
+        earnings = 0;
+    }
+}
